03951d7ca4023426a946c38a85a88b8ecfa06ab6
Karina <say.karina@hotmail.com>
2021-07-23 19:35:55 -0300

    Semana 1 - Todos os vídeos e BD

--- AluraChallengeKarina/App_Start/WebApiConfig.cs
+++ AluraChallengeKarina/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace AluraChallengeKarina
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ AluraChallengeKarina/Controllers/VideosController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using AluraChallengeKarina.Models;
+using Newtonsoft.Json;
+
+namespace AluraChallengeKarina.Controllers
+{
+    public class VideosController : ApiController
+    {
+        public SqlConnection ConnectionString { get; set; }
+
+        public VideosController()
+        {
+            ConnectionString = new SqlConnection(ConfigurationManager.ConnectionStrings["connection-challenge"].ConnectionString);
+        }
+
+        public List<Video> Get()
+        {
+            using (ConnectionString)
+            {
+                ConnectionString.Open();
+                string selecionaTudo = "SELECT * FROM VIDEO";
+
+                SqlCommand cmd = new SqlCommand(selecionaTudo, this.ConnectionString);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                List<Video> videos = new List<Video>();
+
+                while (sdr.Read())
+                {
+                    Video video = new Video();
+                    video.Id = sdr.GetInt32(sdr.GetOrdinal("id"));
+                    video.Titulo = sdr.IsDBNull(sdr.GetOrdinal("titulo")) ? "" : sdr.GetString(sdr.GetOrdinal("titulo"));
+                    video.Descricao = sdr.IsDBNull(sdr.GetOrdinal("descricao")) ? "" : sdr.GetString(sdr.GetOrdinal("descricao"));
+                    video.Url = sdr.IsDBNull(sdr.GetOrdinal("url")) ? "" : sdr.GetString(sdr.GetOrdinal("url"));
+                    
+                    videos.Add(video);
+                }
+
+                return videos;
+            }
+
+        }
+    }
+}
