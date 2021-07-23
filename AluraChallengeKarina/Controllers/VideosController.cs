901e54c4782cdaa8c57b1280c0942df331c7b098
Karina <say.karina@hotmail.com>
2021-07-23 20:42:24 -0300

    Get(id)

--- AluraChallengeKarina/Controllers/VideosController.cs
+++ AluraChallengeKarina/Controllers/VideosController.cs
@@ -45,5 +45,40 @@ namespace AluraChallengeKarina.Controllers
             }
 
         }
+        public Video Get(int id)
+        {
+            Video video = new Video();
+            using (ConnectionString)
+            {
+                ConnectionString.Open();
+                string selecionaTudo = string.Format("SELECT * FROM VIDEO WHERE id={0}", id);
+
+                SqlCommand cmd = new SqlCommand(selecionaTudo, this.ConnectionString);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                List<Video> videos = new List<Video>();
+
+                if (sdr.Read())
+                {
+                    video.Id = sdr.GetInt32(sdr.GetOrdinal("id"));
+                    video.Titulo = sdr.IsDBNull(sdr.GetOrdinal("titulo")) ? "" : sdr.GetString(sdr.GetOrdinal("titulo"));
+                    video.Descricao = sdr.IsDBNull(sdr.GetOrdinal("descricao")) ? "" : sdr.GetString(sdr.GetOrdinal("descricao"));
+                    video.Url = sdr.IsDBNull(sdr.GetOrdinal("url")) ? "" : sdr.GetString(sdr.GetOrdinal("url"));
+                }
+            }
+
+            return video;
+        }
+        
+        public void Post([FromBody] string value)
+        {
+        }
+
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        public void Delete(int id)
+        {
+        }
     }
 }
